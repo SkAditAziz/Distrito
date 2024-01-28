6d2d7f83366afd4be591d4506b93922708359b29
SkAditAziz <skaditaziz@gmail.com>
2024-01-28 22:12:23 +0600

    Added product and category classes

---
+++ src/main/java/com/example/test/model/Product.java
@@ -0,0 +1,14 @@
+package com.example.test.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private List<Category> categories;
+}
